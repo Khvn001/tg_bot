88ea5df8dc49f68f0c816e7e96a952e00ad9a2dc
Khvn001 <khvn001@mail.ru>
2024-09-08 14:49:41 +0500

    DOCKER SECOND SERVER

--- service-client/src/main/java/com/telegrambot/marketplace/configuration/WebConfig.java
+++ service-client/src/main/java/com/telegrambot/marketplace/configuration/WebConfig.java
@@ -1,5 +1,6 @@
 package com.telegrambot.marketplace.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${security.cors_url}")
+    private String CORS_URL;
+
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8000")  // Replace with your frontend's origin
+                .allowedOrigins(CORS_URL)  // Replace with your frontend's origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
