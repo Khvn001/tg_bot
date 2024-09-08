a640c858ef1bb93e54f31a0cd42da05208bc317c
Khvn001 <khvn001@mail.ru>
2024-09-08 17:05:26 +0500

    DOCKER pt.6

--- service-client/src/main/java/com/telegrambot/marketplace/security/SimpleCORSFilter.java
+++ service-client/src/main/java/com/telegrambot/marketplace/security/SimpleCORSFilter.java
@@ -17,7 +17,7 @@ public class SimpleCORSFilter extends HttpFilter {
     private String corsUrl;
 
     @Override
-    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+    protected void doFilter(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain)
             throws IOException, ServletException {
 
         response.setHeader("Access-Control-Allow-Origin", corsUrl);
